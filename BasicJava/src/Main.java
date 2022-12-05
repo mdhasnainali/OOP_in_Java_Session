c6b04d9e38bd22c759e510181480578f39c017f9
mdhasnainali <mdhasnainali.01@gmail.com>
2022-12-05 22:03:27 +0600

    inital steps for transfering

---
+++ BasicJava/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+        int x[] = {7,4,8,9,5};
+        x[0] = 8;
+        System.out.println(x[1]);
+    }
+}
