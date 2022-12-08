22314ed454a6e696296530b9d5775a8af4970b98
mdhasnainali <mdhasnainali.01@gmail.com>
2022-12-08 23:28:40 +0600

    Add Advanced Input Output

---
+++ AdvanceInputOutput/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.Reader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // For Text Input/Output: 2 Class
+        //      i. Readers    # Abstract Class. So need below classes
+        //             BufferedReader
+        //             InputStreamReader   <-  FileReader
+        //             StringReader
+        //
+        //      ii. Writers
+        //             BufferedWriter
+        //             InputStreamWriter <- FileWriter
+        //             StringWriter
+        //             PrintWriter
+
+
+        
+
+
+        // For Binary Input/Output: 2 Class
+        //      i. InputStream
+        //      ii. OutStream
+
+
+
+    }
+}
